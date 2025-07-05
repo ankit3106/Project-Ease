35ba64bf70635d7a8683181a38c03fbefa8581fd
Ankit <120703430+ankit3106@users.noreply.github.com>
2025-07-05 20:53:48 +0530

    Update notificationsRoute.js

--- backend/routes/notificationsRoute.js
+++ backend/routes/notificationsRoute.js
@@ -1,88 +1,3 @@
-// const router = require("express").Router();
-// const Notification = require("../models/notificationsModel");
-// const authMiddleware = require("../middlewares/authMiddleware");
-
-// router.post("/add-notification", authMiddleware, async (req, res) => {
-//   try {
-//     const newNotification = new Notification(req.body);
-//     await newNotification.save();
-//     res.send({
-//       success: true,
-//       data: newNotification,
-//       message: "Notification added successfully",
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-// router.get("/get-all-notifications", authMiddleware, async (req, res) => {
-//   try {
-//     const notifications = await Notification.find({
-//       user: req.userId, 
-//     }).sort({ createdAt: -1 });
-//     res.send({
-//       success: true,
-//       data: notifications,
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-// router.post("/mark-as-read", authMiddleware, async (req, res) => {
-//   try {
-//     await Notification.updateMany(
-//       {
-//         user: req.userId, 
-//         read: false,
-//       },
-//       {
-//         read: true,
-//       }
-//     );
-//     const notifications = await Notification.find({
-//       user: req.userId,
-//     }).sort({ createdAt: -1 });
-//     res.send({
-//       success: true,
-//       message: "Notifications marked as read",
-//       data: notifications,
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-// router.delete("/delete-all-notifications", authMiddleware, async (req, res) => {
-//   try {
-//     await Notification.deleteMany({
-//       user: req.userId, 
-//     });
-//     res.send({
-//       success: true,
-//       message: "All notifications deleted",
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-
-// module.exports = router;
-
 const router = require("express").Router();
 const Notification = require("../models/notificationsModel");
 const authMiddleware = require("../middlewares/authMiddleware");
@@ -171,3 +86,89 @@ router.delete("/delete-all-notifications", authMiddleware, async (req, res) => {
 });
 
 module.exports = router;
+
+// const router = require("express").Router();
+// const Notification = require("../models/notificationsModel");
+// const authMiddleware = require("../middlewares/authMiddleware");
+
+// router.post("/add-notification", authMiddleware, async (req, res) => {
+//   try {
+//     const newNotification = new Notification(req.body);
+//     await newNotification.save();
+//     res.send({
+//       success: true,
+//       data: newNotification,
+//       message: "Notification added successfully",
+//     });
+//   } catch (error) {
+//     res.status(500).send({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
+
+// router.get("/get-all-notifications", authMiddleware, async (req, res) => {
+//   try {
+//     const notifications = await Notification.find({
+//       user: req.userId, 
+//     }).sort({ createdAt: -1 });
+//     res.send({
+//       success: true,
+//       data: notifications,
+//     });
+//   } catch (error) {
+//     res.status(500).send({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
+
+// router.post("/mark-as-read", authMiddleware, async (req, res) => {
+//   try {
+//     await Notification.updateMany(
+//       {
+//         user: req.userId, 
+//         read: false,
+//       },
+//       {
+//         read: true,
+//       }
+//     );
+//     const notifications = await Notification.find({
+//       user: req.userId,
+//     }).sort({ createdAt: -1 });
+//     res.send({
+//       success: true,
+//       message: "Notifications marked as read",
+//       data: notifications,
+//     });
+//   } catch (error) {
+//     res.status(500).send({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
+
+// router.delete("/delete-all-notifications", authMiddleware, async (req, res) => {
+//   try {
+//     await Notification.deleteMany({
+//       user: req.userId, 
+//     });
+//     res.send({
+//       success: true,
+//       message: "All notifications deleted",
+//     });
+//   } catch (error) {
+//     res.status(500).send({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
+
+
+// module.exports = router;
+
