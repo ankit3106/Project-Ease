6573dfd4faf67b3b854d02106954d473e3ce7eb8
Ankit <120703430+ankit3106@users.noreply.github.com>
2025-07-22 18:35:28 +0530

    Update usersRoute.js

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-//  Register User
+
 router.post("/register", async (req, res) => {
   try {
     const userExists = await User.findOne({ email: req.body.email });
@@ -21,15 +21,6 @@ router.post("/register", async (req, res) => {
 
     const user = new User(req.body);
     await user.save();
-
-    //  Emit socket event for new registration (optional)
-    const io = req.app.get("io");
-    io.emit("user-registered", {
-      _id: user._id,
-      name: `${user.firstName} ${user.lastName}`,
-      email: user.email,
-    });
-
     res.status(201).send({
       success: true,
       message: "User registered successfully",
@@ -42,7 +33,6 @@ router.post("/register", async (req, res) => {
   }
 });
 
-//  Login User
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -72,13 +62,6 @@ router.post("/login", async (req, res) => {
     const userObj = user.toObject();
     delete userObj.password;
 
-    //  Emit socket event for login (optional)
-    const io = req.app.get("io");
-    io.to(user._id.toString()).emit("user-logged-in", {
-      userId: user._id,
-      name: `${user.firstName} ${user.lastName}`,
-    });
-
     res.status(200).send({
       success: true,
       data: {
@@ -95,7 +78,6 @@ router.post("/login", async (req, res) => {
   }
 });
 
-//  Get Logged-In User Info
 router.get("/get-logged-in-user", authMiddleware, async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.userId }).select("-password");
@@ -113,102 +95,4 @@ router.get("/get-logged-in-user", authMiddleware, async (req, res) => {
 });
 
 module.exports = router;
-
-// const router = require("express").Router();
-// const User = require("../models/userModel");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const authMiddleware = require("../middlewares/authMiddleware");
-
-
-// router.post("/register", async (req, res) => {
-//   try {
-//     const userExists = await User.findOne({ email: req.body.email });
-//     if (userExists) {
-//       return res.status(400).send({
-//         success: false,
-//         message: "User already exists",
-//       });
-//     }
-
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-//     req.body.password = hashedPassword;
-
-//     const user = new User(req.body);
-//     await user.save();
-//     res.status(201).send({
-//       success: true,
-//       message: "User registered successfully",
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-// router.post("/login", async (req, res) => {
-//   try {
-//     const user = await User.findOne({ email: req.body.email });
-//     if (!user) {
-//       return res.status(400).send({
-//         success: false,
-//         message: "User does not exist",
-//       });
-//     }
-
-//     const passwordCorrect = await bcrypt.compare(
-//       req.body.password,
-//       user.password
-//     );
-//     if (!passwordCorrect) {
-//       return res.status(400).send({
-//         success: false,
-//         message: "Invalid password",
-//       });
-//     }
-
-//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-//       expiresIn: "1d",
-//     });
-
-//     // Remove password before sending user info
-//     const userObj = user.toObject();
-//     delete userObj.password;
-
-//     res.status(200).send({
-//       success: true,
-//       data: {
-//         token,
-//         user: userObj,
-//       },
-//       message: "User logged in successfully",
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-// router.get("/get-logged-in-user", authMiddleware, async (req, res) => {
-//   try {
-//     const user = await User.findOne({ _id: req.userId }).select("-password");
-//     res.status(200).send({
-//       success: true,
-//       data: user,
-//       message: "User fetched successfully",
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-// module.exports = router;
 
